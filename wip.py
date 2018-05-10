8886b0e08347f161bee21ca8e935d34f3fc06487
arleeninclan <31894506+arleeninclan@users.noreply.github.com>
2018-05-10 11:24:23 -0400

    Tries Left and More Words
    I added a couple more words to guess. The user will have double the amount of letters in the word as tries left (I might decrease it). It does not take off a try if you guess a letter correctly.

--- wip.py
+++ wip.py
@@ -12,9 +12,11 @@ def search(iterable, obj):
   
 class Hangman:
   
-  WORDS = ['psychology', 'network', 'biology', 'tulips', 'highschool', 'transcript', 'pneumonia', 'oxygen', 'syndrome', 'wristwatch', 'policeman', 'zodiac', 'microwave', 'transplant','subway', 'transportation', 'celebration', 'fishhook', 'blizzard', 'hurricane', 'tsunami', 'earthquake', 'bookwarm', 'jazz', 'crocodile', 'venus', 'xray', 'itching', 'sleeveless', 'foxes', 'overjoy', 'puzzling', 'python', 'engineering']
+  WORDS = ['psychology', 'network', 'biology', 'tulips', 'highschool', 'transcript', 'university', 'pneumonia', 'oxygen', 'syndrome', 'wristwatch', 'policeman', 'zodiac', 'microwave', 'transplant','subway', 'transportation', 'celebration', 'fishhook', 'blizzard', 'hurricane', 'tsunami', 'earthquake', 'bookworm', 'temperature', 'potion', 'weather', 'computer', 'classic', 'music', 'literature', 'trigonometry', 'calculus', 'chemistry', 'physics', 'javascript', 'jazz', 'crocodile', 'venus', 'xray', 'itching', 'sleeveless', 'foxes', 'overjoy', 'puzzling', 'python', 'engineering', 'country', 'anatomy', 'synonym','optics', 'lipstick', 'recreation', 'applecider', 'vegetable', 'homework', 'exam', 'violence', 'protest', 'government', 'pangaea', 'earth', 'staircase', 'classroom', 'message', 'security', 'fishtank', 'pineapple', 'jacket', 'shower', 'clarinet', 'flute', 'xray', 'award', 'hangman', 'synapsis', 'mitosis', 'earings', 'boots', 'parrot', 'godfather', 'wedding', 'celebration', 'birthday', 'garden', 'computer', 'consequence']
   
   chosen_word = list(random.choice(WORDS))
+  
+  tries_left = len(chosen_word)*2
   
   def __init__(self):
     self.state = ['_' for i in self.chosen_word]
@@ -32,10 +34,13 @@ class Hangman:
     index = search(self.chosen_word, check_letter)
     for n in index:
       self.state[n] = self.chosen_word[n]
-      
+    if index == []:
+      self.tries_left -= 1
+  
   def has_won(self): 
     if is_subset(self.chosen_word, self.state):
       return True
+    
     
   def play(self):
     while not self.game_over:
@@ -43,7 +48,11 @@ class Hangman:
       if self.has_won():
         print('You won!')
         self.game_over = True
+        together = ''.join(self.chosen_word)
+        print(together)
         break
+      tries = 'You have {} tries left'.format(self.tries_left)
+      print(tries)
   
 
 #-------------END-------------#
