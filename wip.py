e8a2a18ab5039cf1a3042689d52b4f6a91865f09
arleeninclan <31894506+arleeninclan@users.noreply.github.com>
2018-05-14 11:24:03 -0400

    Number Input Error and Spaces
    If the user inputs a number, it won't take off a try off of its total tries. I also changed it to there being 10 tries in total. I also added spaces to make it more clearer for the user to see what is going on when the code is being run.

--- wip.py
+++ wip.py
@@ -12,11 +12,11 @@ def search(iterable, obj):
   
 class Hangman:
   
-  WORDS = ['psychology', 'network', 'biology', 'tulips', 'highschool', 'transcript', 'university', 'pneumonia', 'oxygen', 'syndrome', 'wristwatch', 'policeman', 'zodiac', 'microwave', 'transplant','subway', 'transportation', 'celebration', 'fishhook', 'blizzard', 'hurricane', 'tsunami', 'earthquake', 'bookworm', 'temperature', 'potion', 'weather', 'computer', 'classic', 'music', 'literature', 'trigonometry', 'calculus', 'chemistry', 'physics', 'javascript', 'jazz', 'crocodile', 'venus', 'xray', 'itching', 'sleeveless', 'foxes', 'overjoy', 'puzzling', 'python', 'engineering', 'country', 'anatomy', 'synonym','optics', 'lipstick', 'recreation', 'applecider', 'vegetable', 'homework', 'exam', 'violence', 'protest', 'government', 'pangaea', 'earth', 'staircase', 'classroom', 'message', 'security', 'fishtank', 'pineapple', 'jacket', 'shower', 'clarinet', 'flute', 'xray', 'award', 'hangman', 'synapsis', 'mitosis', 'earings', 'boots', 'parrot', 'godfather', 'wedding', 'celebration', 'birthday', 'garden', 'computer', 'consequence']
+  WORDS = ['psychology', 'network', 'biology', 'tulips', 'highschool', 'transcript', 'university', 'pneumonia', 'oxygen', 'syndrome', 'wristwatch', 'policeman', 'zodiac', 'microwave', 'transplant','subway', 'transportation', 'celebration', 'fishhook', 'blizzard', 'hurricane', 'tsunami', 'earthquake', 'bookworm', 'temperature', 'potion', 'weather', 'computer', 'classic', 'music', 'literature', 'trigonometry', 'calculus', 'chemistry', 'physics', 'javascript', 'jazz', 'crocodile', 'venus', 'xray', 'itching', 'sleeveless', 'foxes', 'overjoy', 'puzzling', 'python', 'engineering', 'country', 'anatomy', 'synonym','optics', 'lipstick', 'recreation', 'applecider', 'vegetable', 'homework', 'exam', 'violence', 'protest', 'government', 'pangaea', 'earth', 'staircase', 'classroom', 'message', 'security', 'fishtank', 'pineapple', 'jacket', 'shower', 'clarinet', 'flute', 'xray', 'award', 'hangman', 'synapsis', 'mitosis', 'earings', 'boots', 'parrot', 'godfather', 'wedding', 'celebration', 'birthday', 'garden', 'computer', 'consequence', 'education', 'manufacture', 'company']
   
   chosen_word = list(random.choice(WORDS))
   
-  tries_left = len(chosen_word)*2
+  tries_left = 10
   
   def __init__(self):
     self.state = ['_' for i in self.chosen_word]
@@ -28,14 +28,25 @@ class Hangman:
   def __str__(self):
     return 
   
+  def checking(self, obj):
+    try:
+      float(obj)
+    except ValueError:
+      return False
+    return True
+    
+  #INCORPORATE CHECKING FUNCTION INTO MOVE FUNCTION
+  
   def move(self):
     print(self.state)
-    check_letter = input("Guess a letter... make sure it's lowercase: ")
-    index = search(self.chosen_word, check_letter)
-    for n in index:
-      self.state[n] = self.chosen_word[n]
-    if index == []:
-      self.tries_left -= 1
+    check_letter = input("Guess a letter: ")
+    check_letter = check_letter.lower()
+    if self.checking(check_letter) == False:
+      index = search(self.chosen_word, check_letter)
+      for n in index:
+        self.state[n] = self.chosen_word[n]
+      if index == []:
+        self.tries_left -= 1
   
   def has_won(self): 
     if is_subset(self.chosen_word, self.state):
@@ -46,19 +57,20 @@ class Hangman:
     while not self.game_over:
       self.move()
       if self.has_won():
-        print('You won!')
+        print('\nYou won!')
         self.game_over = True
         together = ''.join(self.chosen_word)
         print('The word was: {}'.format(together))
         break
-      tries = 'You have {} tries left'.format(self.tries_left)
       if self.tries_left == 0:
         self.game_over = True
         together = ''.join(self.chosen_word)
-        print('You lose! ):')
+        print('\nYou lose! ):')
         print('The word was: {}'.format(together))
         break
+      tries = 'You have {} tries left\n'.format(self.tries_left)
       print(tries)
+    
   
 
 #-------------END-------------#
